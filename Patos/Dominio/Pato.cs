d6b2daf3e6b5f560c37ebddd909eff1ef5cf765c
Mahmoud Ali <muddibr@gmail.com>
2020-08-16 20:30:03 -0300

    Inicial

---
+++ Patos/Dominio/Pato.cs
@@ -0,0 +1,12 @@
+﻿namespace Patos.Dominio
+{
+    public class Pato
+    {
+        public Pato(string imagem)
+        {
+            Imagem = imagem;
+        }
+
+        public string Imagem { get; set; }
+    }
+}
---
+++ Patos/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Patos
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            _environment = environment;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var mvcBulder = services.AddControllersWithViews();
+
+#if DEBUG
+            if (_environment.IsDevelopment())
+            {
+                mvcBulder.AddRazorRuntimeCompilation();
+            }
+#endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
